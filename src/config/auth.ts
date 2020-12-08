991182f7e451f3e9fe6538e3716ae6360741fa28
Juangomes07 <72666763+Juangomes07@users.noreply.github.com>
2020-12-08 16:14:05 -0300

    Initial commit

---
+++ src/config/auth.ts
@@ -0,0 +1,6 @@
+export default {
+  jwt: {
+    secret: '9c76b636de287e96cdb2058c30041a09',
+    expiresIn: '99d',
+  },
+};
---
+++ src/services/CreateAppointmentService.ts
@@ -0,0 +1,45 @@
+import Appointment from '../models/Appointment';
+import AppointmentsRepository from '../repositories/AppointmentsRepository';
+import {startOfHour} from 'date-fns';
+import {getCustomRepository} from 'typeorm';
+
+import AppError from '../errors/AppError';
+
+/*
+  [x]Recebimento das informaçãos
+  [/]Tratativa de erros/excessões
+  [x]Acesso ao repositório
+*/
+
+interface Request{
+  provider_id: string;
+  date: Date;
+}
+
+/*
+Dependency Inversio (SOLID)
+ */
+
+class CreateAppointmentService{
+  public async execute({date, provider_id}:Request): Promise<Appointment> {
+  const appointmentsRepository =  getCustomRepository(AppointmentsRepository);
+
+  const appointmentDate = startOfHour(date);
+
+  const findAppointmentInSameDate = await appointmentsRepository.findByDate(
+    appointmentDate,);
+
+    if(findAppointmentInSameDate) {
+      throw new AppError('This appointment has already booked')
+    }
+
+    const appointment= appointmentsRepository.create({
+      provider_id,
+      date: appointmentDate,
+    });
+
+    await appointmentsRepository.save(appointment);
+    return appointment;
+  }
+}
+export default CreateAppointmentService;
