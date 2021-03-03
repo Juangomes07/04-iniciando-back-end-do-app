86b2cf735d02cd2262b7dbf0f647c55129aa9049
Juangomes07 <72666763+Juangomes07@users.noreply.github.com>
2021-03-03 20:50:59 -0300

    Utilizando class transformer

--- src/modules/users/infra/http/controllers/ProfileController.ts
+++ src/modules/users/infra/http/controllers/ProfileController.ts
@@ -1,5 +1,6 @@
 import {Request, Response } from 'express';
 import { container } from 'tsyringe'
+import {classToClass} from 'class-transformer';
 
 import UpdateProfileService from '@modules/users/services/UpdateProfileService';
 import ShowProfileService from '@modules/users/services/ShowProfileService';
@@ -14,16 +15,16 @@ export default class ProfileController {
     const user = await showProfile.execute({user_id});
 
     // Com a atualização do TypeScript, isso se faz necessário
-    const userWithoutPassword = {
-      id: user.id,
-      name: user.name,
-      email: user.email,
-      avatar: user.avatar,
-      created_at: user.created_at,
-      updated_at: user.updated_at,
-    };
+    // const userWithoutPassword = {
+    //   id: user.id,
+    //   name: user.name,
+    //   email: user.email,
+    //   avatar: user.avatar,
+    //   created_at: user.created_at,
+    //   updated_at: user.updated_at,
+    // };
 
-    return response.json(userWithoutPassword);
+    return response.json(classToClass(user));
   }
 
   public async update(request: Request, response: Response): Promise<Response> {
@@ -39,16 +40,8 @@ export default class ProfileController {
       password,
     });
 
-    // Com a atualização do TypeScript, isso se faz necessário
-    const userWithoutPassword = {
-      id: user.id,
-      name: user.name,
-      email: user.email,
-      created_at: user.created_at,
-      updated_at: user.updated_at,
-    };
 
-    return response.json(userWithoutPassword);
+    return response.json(classToClass(user));
   }
 
 
